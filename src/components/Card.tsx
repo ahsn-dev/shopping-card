1da2254b353280cf5dd211ba2fbd112adb68190f
ahsn.dev <ahsn.dev@gmail.com>
2023-05-04 16:44:07 +0330

    complete cart component

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -4,17 +4,19 @@ interface Props {
 
 const Card = ({ product }: Props) => {
   return (
-    <div className="text-black w-full h-full rounded border border-black">
-      <img src="https://picsum.photos/200/300" alt="" />
-      <div>
-        <p>{product.title}</p>
+    <div className="text-black w-full h-full rounded border border-gray-300">
+      <img src="https://picsum.photos/350/200" alt="" className="rounded-t" />
+      <div className="p-4 flex flex-col">
+        <h3 className="text-xl font-medium">{product.title}</h3>
         <p>
           ₹<span>{product.price}</span>
         </p>
         <p>{product.delivery}</p>
         <div></div>
-        <button className="bg-blue-500 px-4 py-2 text-white rounded">
-          {product.inStock ? "Add to Cart" : "Out of Stock"}
+      </div>
+      <div className="p-4">
+        <button className="bg-blue-500 text-white rounded py-2 px-3 font-light">
+          {product.outof ? "Add to Cart" : "Out of Stock"}
         </button>
       </div>
     </div>
